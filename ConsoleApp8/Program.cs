b77d41e7dc76aa36da41e0445b51f69f345ec9d0
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-12 19:08:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void N(int n, int a)
+        {
+            if (a <= n)
+            {
+                for (int j = 1; j <= a; j++)
+                    Console.Write(a + " ");
+                Console.WriteLine();
+                N(n, a + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите n: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if (n > 0)
+                { N(n, 1); }
+                else Console.Write("Число должно быть больше 0!");
+            }
+            catch { Console.Write("Некорректный ввод!"); }
+        }
+        
+    }
+}
